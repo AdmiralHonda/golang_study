1eba5aee7a1f4e4cc2c1c8d574cb65f3b14f9fff
AdmiralHonda <ceta17163@g.nihon-u.ac.jp>
2021-03-18 16:08:27 +0900

    [r03]3/18

--- go_lang/go.mod
+++
@@ -1,5 +0,0 @@
-module iok_server/v1
-
-go 1.13
-
-require github.com/AdmiralHonda/iok v0.0.0-20210304150539-da8d0cbbce7b
--- go_lang/main.go
+++ go_lang/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-	"github.com/AdmiralHonda/iok/iok_packeage/handler_logic"
 	"log"
 	"net/http"
+	"path/filepath"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", handler_logic.SayhelloName) //アクセスのルーティングを設定します。
-	err := http.ListenAndServe(":9090", nil)         //監視するポートを設定します。
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+// templ represents a single template
+type templateHandler struct {
+	filename string
+	templ    *template.Template
+}
+
+// Handle is a http.HandleFunc that renders this template.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if t.templ == nil {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	}
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	r := newRoom()
+
+	http.Handle("/", (&templateHandler{filename: "chat.html"}))
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
+
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
 }
