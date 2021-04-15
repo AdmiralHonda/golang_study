84a96de97de19c8cbd4f00dc16a18650b1c22c9c
AdmiralHonda <ceta17163@g.nihon-u.ac.jp>
2021-04-15 15:56:20 +0900

    [update]fit ws

--- go_lang/main.go
+++ go_lang/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -18,19 +19,25 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if t.templ == nil {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	}
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
+var addr = flag.String("addr", ":8080", "アプリのポート番号")
+
 func main() {
+
+	flag.Parse()
+
 	r := newRoom()
 
 	http.Handle("/", (&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
+	log.Println("Webサーバーを開始します。:", *addr)
 
 	// start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
